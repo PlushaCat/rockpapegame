0328696af74097028293ad4bab7a51d78b8cc9ab
Sosu <rgr>
2023-10-23 14:03:58 +0300

    132

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,61 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    game()
+
+
+}
+
+fun Checkresult(input: String) {
+    var randomvar = Random.nextInt(1, 3)
+    var vrag = ""
+
+    when (randomvar) {
+        1 -> vrag = "Камень"
+        2 -> vrag = "Ножницы"
+        3 -> vrag = "Бумага"
+    }
+
+    if (input == vrag) {
+        println("Вы  выбрали " + input + ", соперник выбрал " + vrag + " Ничья")
+        game()
+    }
+
+    if (input == "Камень" && vrag == "Ножницы") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы победили")
+    }
+
+    if (input == "Камень" && vrag == "Бумага") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы проиграли")
+    }
+
+    if (input == "Ножницы" && vrag == "Камень") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы проиграли")
+    }
+
+    if (input == "Ножницы" && vrag == "Бумага") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы победили")
+    }
+
+    if (input == "Бумага" && vrag == "Ножницы") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы проиграли")
+    }
+
+    if (input == "Бумага" && vrag == "Камень") {
+        println("Вы выбрали " + input + ", соперник выбрал " + vrag + " Вы победили")
+    }
+
+}
+
+
+fun game() {
+    println("Введите 'Каменьfrgh', 'Ножницы', 'Бумага'.")
+
+    var v = readln()
+
+    when (v) {
+        "Камень" -> Checkresult("Камень")
+        "Ножницы" -> Checkresult("Ножницы")
+        "Бумага" -> Checkresult("Бумага")
+    }
 }
